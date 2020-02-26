8779740112e63274064921b7716b10c20975a33a
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-26 15:48:44 +0400

    made main-section

---
+++ src/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Main.module.css';
+
+const Main= ()=> {
+	return (
+		<div className={styles.main}>
+			<div className={styles.main_container}>
+				<div className={styles.main_wrap}></div>
+			</div>
+		</div>
+	);
+}
+
+export default Main;
