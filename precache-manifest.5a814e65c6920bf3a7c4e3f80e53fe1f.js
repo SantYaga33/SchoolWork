f51602accdbf9dcb91c744b2807355f0397d9590
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-03-20 18:55:56 +0400

    Updates

--- precache-manifest.5a814e65c6920bf3a7c4e3f80e53fe1f.js
+++ precache-manifest.5a814e65c6920bf3a7c4e3f80e53fe1f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c494830e55bea69ff0671e287abc9c8d",
+    "revision": "6531257a2a0b66bbc08688846eb57298",
     "url": "/SchoolWork/index.html"
   },
   {
-    "revision": "88f7e7e86c44f76c5ea6",
-    "url": "/SchoolWork/static/css/main.009dd256.chunk.css"
+    "revision": "bded12e761e8c6f5b312",
+    "url": "/SchoolWork/static/css/main.57636cd6.chunk.css"
   },
   {
     "revision": "ed87c08a85fe389615f1",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SchoolWork/static/js/2.603386f2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "88f7e7e86c44f76c5ea6",
-    "url": "/SchoolWork/static/js/main.71832ceb.chunk.js"
+    "revision": "bded12e761e8c6f5b312",
+    "url": "/SchoolWork/static/js/main.d06452e0.chunk.js"
   },
   {
     "revision": "771c3669135fd2f6731b",
