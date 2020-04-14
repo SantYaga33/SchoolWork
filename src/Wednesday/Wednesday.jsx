f99b378a6403b91cf8659623f59ba4311e02c673
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-04-14 18:47:30 +0400

    add radio btn for wednesday

--- src/Wednesday/Wednesday.jsx
+++ src/Wednesday/Wednesday.jsx
@@ -4,26 +4,64 @@ import styles from './Wednesday.module.css'
 const Wednesday = () => {
 
 	return (
-		<div className={styles.main}>
-			<div className={styles.switch__wrapper}>
-				<div className={styles.switch}>
-					<input name="switch" id="one" type="radio" checked/>
+		<div className={styles.wednesday__bg}>
 
+			<div className={`${styles.wednesday__border} ${styles.border__one}`}>
+				<div className={`${styles.wednesday__border} ${styles.border__two}`}>
 
-
-
-					<label htmlFor="one" className={styles.switch__label}>One</label>
-
-					<input name="switch" id="two" type="radio"/>
-					<label htmlFor="two" className={styles.switch__label}>Two</label>
-					<input name="switch" id="three" type="radio"/>
-					<label htmlFor="three" className={styles.switch__label}>Three</label>
-					<div className={styles.switch__indicator}></div>
 				</div>
 			</div>
+			<div className={styles.continput__wrap}>
+				<div className={styles.continput}>
+					<ul className={styles.continput__ul}>
+						<li className={styles.continput__li}>
+							<input checked type="radio" name="1" id='one'/>
+							<label htmlFor='one'>green theme</label>
+							<div className={styles.bullet}>
+								<div className={`${styles.line} ${styles.zero}`}></div>
+								<div className={`${styles.line} ${styles.one}`}></div>
+								<div className={`${styles.line} ${styles.two}`}></div>
+								<div className={`${styles.line} ${styles.three}`}></div>
+								<div className={`${styles.line} ${styles.four}`}></div>
+								<div className={`${styles.line} ${styles.five}`}></div>
+								<div className={`${styles.line} ${styles.six}`}></div>
+								<div className={`${styles.line} ${styles.seven}`}></div>
+							</div>
+						</li>
+						<li className={styles.continput__li}>
+							<input type="radio" name="1" id='two'/>
+							<label htmlFor='two'>dark theme </label>
+							<div className={styles.bullet}>
+								<div className={`${styles.line} ${styles.zero}`}></div>
+								<div className={`${styles.line} ${styles.one}`}></div>
+								<div className={`${styles.line} ${styles.two}`}></div>
+								<div className={`${styles.line} ${styles.three}`}></div>
+								<div className={`${styles.line} ${styles.four}`}></div>
+								<div className={`${styles.line} ${styles.five}`}></div>
+								<div className={`${styles.line} ${styles.six}`}></div>
+								<div className={`${styles.line} ${styles.seven}`}></div>
+							</div>
+						</li>
+						<li className={styles.continput__li}>
+							<input type="radio" name="1" id='three'/>
+							<label htmlFor='three'>light theme </label>
+							<div className={styles.bullet}>
+								<div className={`${styles.line} ${styles.zero}`}></div>
+								<div className={`${styles.line} ${styles.one}`}></div>
+								<div className={`${styles.line} ${styles.two}`}></div>
+								<div className={`${styles.line} ${styles.three}`}></div>
+								<div className={`${styles.line} ${styles.four}`}></div>
+								<div className={`${styles.line} ${styles.five}`}></div>
+								<div className={`${styles.line} ${styles.six}`}></div>
+								<div className={`${styles.line} ${styles.seven}`}></div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+
 		</div>
 	)
-
 
 };
 
